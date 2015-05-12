2959985d018e142c78aa46f6b5776cc8f591398a
iamnailbiter <iam.nailbiter@gmail.com>
2015-05-12 12:44:59 +0700

    test.php

--- test.php
+++ test.php
@@ -1,8 +1,19 @@
 <?php
 
+require_once 'Class/Gammu.php';
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+$gammu_bin 		= '/usr/bin/gammu-smsd';
+$gammu_config 		= '/etc/gammu-smsdrc';
+$gammu_config_section	= ''; // for default section please set "blank" value --> $gammu_config_section = '';
+
+$sms = new Gammu($gammu_bin,$gammu_config,$gammu_config_section);
+
+$response = $sms->phoneBook('ME');
+    echo '<pre>';
+    print_r($response);
+    echo '</pre>';
